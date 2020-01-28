dba4f2efa960c02acdd18fde0e9edf17de5134a0
Robin Sliwa <rorrosliwa@gmail.com>
2020-01-28 17:27:34 +0100

    Change to pymouse

--- track_mouse.py
+++ track_mouse.py
@@ -1,29 +1,25 @@
-from Xlib import display
+from pymouse import PyMouse
 
 SAVE_FILE = "mouse_positions.txt"
-SAVE_BEFORE_WRITE = 1000
 
 def main():
+    mouse = PyMouse()
+
+    sf = open(SAVE_FILE, "a")
+    tracked = 0
+
     last_pos = (-1,-1)
-    last_1000 = []
     while last_pos != (0,0):
-        data = display.Display().screen().root.query_pointer()._data
-        mouse_x = data["root_x"]
-        mouse_y = data["root_y"]
+        mouse_x, mouse_y = mouse.position()
 
         if last_pos != (mouse_x, mouse_y):
+            print("x = %i y = %i n=%i" % (mouse_x,mouse_y,tracked))
+
+            sf.write(str(mouse_x)+","+str(mouse_y)+"\n")
             last_pos = (mouse_x, mouse_y)
-            if last_pos not in last_1000:
-                last_1000.append(last_pos)
+            tracked+=1
 
-            print("x = %i y = %i n=%i" % (mouse_x,mouse_y,len(last_1000)))
-
-        if len(last_1000) == SAVE_BEFORE_WRITE:
-            sf = open(SAVE_FILE, "a")
-            for pos in last_1000:
-                sf.write(str(pos[0])+","+str(pos[1])+"\n")
-            sf.close()
-            last_1000.clear()
+    sf.close()
 
 if __name__ == '__main__':
     main()
